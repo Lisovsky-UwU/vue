91a5b8b32ced92e4aae0ba9674bc327adfbc42f5
Некит программист < @DESKTOP-0N9MUS7>
2022-12-05 01:38:37 +0700

    Главная страниц

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
+    component: () => import('@/views/MainView')
   }
 ]
 
---
+++ src/views/MainView.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-content class="mt-10" style="height: calc(100% - 40px);">
+		<v-row no-gutters style="height: 100%;">
+			<v-col cols="12" md="6">
+				<v-card min-height="100%" outlined style="position: relative" class="rounded-lg mr-7" color="element_background">
+					<v-card-text>
+						text text text text
+					</v-card-text>
+				</v-card>
+			</v-col>
+
+			<v-col cols="12" md="6">
+				<v-card min-height="100%" outlined style="position: relative" class="rounded-lg ml-7" color="element_background">
+					<v-card-text>
+						text text text text
+					</v-card-text>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-content>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
